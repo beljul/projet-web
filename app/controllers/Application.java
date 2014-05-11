115192470764a9764df6e9efb6d340133f363b93
beljul <jullian.bellino@gmail.com>
2014-05-11 22:43:01 +0200

    Login page + play secure module

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,19 @@
 package controllers;
 
 import play.*;
+
 import play.mvc.*;
 
 import java.util.*;
 
+import org.codehaus.groovy.control.customizers.SecureASTCustomizer;
+
 import models.*;
 
+@With(Secure.class)
 public class Application extends Controller {
 
     public static void index() {
         render();
-        //comment test 2
     }
-    
 }
