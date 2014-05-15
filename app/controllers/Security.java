b4fc2c8da0e8330698ee6e269ff6049a5c20b970
kevanescence <kevanescence@hotmail.fr>
2014-05-15 13:33:42 +0200

    Initialize log in to dashboard

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -6,9 +6,11 @@ public class Security extends Secure.Security {
 	
 	public static boolean authenticate (String email, String password) {
 		return models.User.connect(email, password) != null;
+	}			
+	static void onAuthenticated(){
+		Application.dashboard();
 	}
-	
 	static void onDisconnected() {
-	    Application.index();
+	    redirect("/");
 	}
 }
