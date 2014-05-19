bd57ae4e676881bf3bd44a23cdc69b80d3b75b8c
kevanescence <kevanescence@hotmail.fr>
2014-05-19 17:48:00 +0200

    Last products

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,12 @@
 package controllers;
 
 import play.*;
-
 import play.mvc.*;
 
 import java.util.*;
 
-
 import models.*;
+import models.Product;
 
 @With(Secure.class)
 public class Application extends Controller {
@@ -19,7 +18,7 @@ public class Application extends Controller {
     @Before
     static void getUserInfo() {
     	models.User connected = models.User.getByEmail(session.get("username"));
-    	List<models.Product> products = connected.getLastProducts();
+    	Collection<Product> products = connected.getProducts().values();
     	renderArgs.put("products", products);       
     }
     
