f91eabf28e86a586a6ff6ad660209c35b06b3dd0
kevanescence <kevanescence@hotmail.fr>
2014-05-21 14:05:14 +0200

    Nouvelle exigence terminée

--- public/javascripts/requirement-add.js
+++ public/javascripts/requirement-add.js
@@ -14,28 +14,46 @@ $('form').on("keydown",'input.requirement-req',function(e){
 								.prev("label")
 								.attr("for","req" + reqCounters)
 								.text("Exigence " + reqCounters);
+			console.log($("div.slider"));
 	        newGrp.find("div.slider").remove();
-			var select1 = newGrp.find("select.slider-priority")
-				  			.attr("id","req-priority-" + reqCounters);
-			var select2 = newGrp.find("select.slider-duration")
-				  .attr("id","req-duration-" + reqCounters);
-			var slider1 = $("<div id=\"slider" + reqCounters + "\"></div>")
+			var select1 = newGrp.find("select.slider-duration")
+				  				.attr("id","req-priority-" + reqCounters);
+			var select2 = newGrp.find("select.slider-priority")
+				  				.attr("id","req-duration-" + reqCounters);
+			var slider1 = $("<div class=\"slider\" id=\"slider" + reqCounters + "1\"></div>")
 							.insertAfter(select1).slider( {
 								min : 1,
 								max : 7,	
 								range : "min",
 								value : select1[0].selectedIndex + 1,
 								slide : function(event, ui) {
+									console.log(select1);
 								select1[0].selectedIndex = ui.value - 1;
 							}
 							});
+			select1.change(function() {
+				console.log()
+				slider1.slider("value", this.selectedIndex + 1);
+			});
+			var slider2 = $("<div id=\"slider" + reqCounters + "2\" class=\"slider\"></div>").insertAfter(select2).slider( {
+				min : 1,
+				max : 5,	
+				range : "min",
+				value : select2[0].selectedIndex + 1,
+				slide : function(event, ui) {
+					select2[0].selectedIndex = ui.value - 1;
+				}
+			});
+			select2.change(function() {
+				slider2.slider("value", this.selectedIndex + 1);
+			});
 			grp.after(newGrp);
 			newReq.focus();
 		}
 	}	
 });
 var select1 = $( ".slider-duration" );
-var slider1 = $("<div id=\"slider1\ class=\"slider\"></div>").insertAfter(select1).slider( {
+var slider1 = $("<div id=\"slider1\" class=\"slider\"></div>").insertAfter(select1).slider( {
 	min : 1,
 	max : 7,	
 	range : "min",
@@ -44,8 +62,11 @@ var slider1 = $("<div id=\"slider1\ class=\"slider\"></div>").insertAfter(select
 		select1[0].selectedIndex = ui.value - 1;
 	}
 });
+select1.change(function() {
+	slider1.slider("value", this.selectedIndex + 1);
+});
 var select2 = $(".slider-priority");
-var slider2 = $("<div id=\"slider2\"class=\"slider\"></div>").insertAfter(select2).slider( {
+var slider2 = $("<div id=\"slider2\" class=\"slider\"></div>").insertAfter(select2).slider( {
 	min : 1,
 	max : 5,	
 	range : "min",
@@ -53,4 +74,7 @@ var slider2 = $("<div id=\"slider2\"class=\"slider\"></div>").insertAfter(select
 	slide : function(event, ui) {
 		select2[0].selectedIndex = ui.value - 1;
 	}
+});
+select2.change(function() {
+	slider2.slider("value", this.selectedIndex + 1);
 });
