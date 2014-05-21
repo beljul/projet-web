b7d7d16556cdc483bb5d501b0bae294c7147a46f
kevanescence <kevanescence@hotmail.fr>
2014-05-21 09:25:56 +0200

    Add a requirement design + JS

--- public/javascripts/requirement-add.js
+++ public/javascripts/requirement-add.js
@@ -4,16 +4,53 @@ $('form').on("keydown",'input.requirement-req',function(e){
 		e.preventDefault();
 		e.stopPropagation();
 		if($(this).val() != "") {
-			var inputGrp = $(this).parent();
-			var newGrp = inputGrp.clone();
+			var grp = $(this).parent().parent();
+			var newGrp = grp.clone();
 			/*Place label link on the new input */
-			inputGrp.prev("label").attr("for","req" + (++reqCounters));			
-			var newReq = newGrp.find("input").val("").attr("id","req" + reqCounters)
-					  .prev("label")
-					  .attr("for","req" + reqCounters)
-					  .text("Exigence " + reqCounters);			
-			inputGrp.after(newGrp);
+			grp.prev("label").attr("for","req" + (++reqCounters));			
+			var newReq = newGrp.find("input")
+								.val("")
+								.attr("id","req" + reqCounters)
+								.prev("label")
+								.attr("for","req" + reqCounters)
+								.text("Exigence " + reqCounters);
+	        newGrp.find("div.slider").remove();
+			var select1 = newGrp.find("select.slider-priority")
+				  			.attr("id","req-priority-" + reqCounters);
+			var select2 = newGrp.find("select.slider-duration")
+				  .attr("id","req-duration-" + reqCounters);
+			var slider1 = $("<div id=\"slider" + reqCounters + "\"></div>")
+							.insertAfter(select1).slider( {
+								min : 1,
+								max : 7,	
+								range : "min",
+								value : select1[0].selectedIndex + 1,
+								slide : function(event, ui) {
+								select1[0].selectedIndex = ui.value - 1;
+							}
+							});
+			grp.after(newGrp);
 			newReq.focus();
 		}
 	}	
+});
+var select1 = $( ".slider-duration" );
+var slider1 = $("<div id=\"slider1\ class=\"slider\"></div>").insertAfter(select1).slider( {
+	min : 1,
+	max : 7,	
+	range : "min",
+	value : select1[0].selectedIndex + 1,
+	slide : function(event, ui) {
+		select1[0].selectedIndex = ui.value - 1;
+	}
+});
+var select2 = $(".slider-priority");
+var slider2 = $("<div id=\"slider2\"class=\"slider\"></div>").insertAfter(select2).slider( {
+	min : 1,
+	max : 5,	
+	range : "min",
+	value : select2[0].selectedIndex + 1,
+	slide : function(event, ui) {
+		select2[0].selectedIndex = ui.value - 1;
+	}
 });
