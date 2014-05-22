e1607da2630a30dcc23b57081970f3526b409a58
beljul <jullian.bellino@gmail.com>
2014-05-22 15:59:36 +0200

    merge

--- app/controllers/User.java
+++ app/controllers/User.java
@@ -6,7 +6,6 @@ import java.util.List;
 import play.mvc.Controller;
 import play.mvc.With;
 
-@With(Secure.class)
 public class User extends Controller {
 
 	public static void register(String email, String name, String firstname, 
