9f8652d14295155dfc7d3a5f22cc2b07774e7824
beljul <jullian.bellino@gmail.com>
2014-05-23 15:17:53 +0200

    Version controller added - sorry.

---
+++ app/controllers/Version.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import play.mvc.With;
+
+@With(Secure.class)
+public class Version extends WrapperController {
+	
+	public static void add() {
+		render();
+	}
+	
+	public static void register(String release, Integer nbSprints) {
+		System.out.println(release);
+		System.out.println(nbSprints);
+		String productName = session.get("productName");
+		models.Product product = models.Product.getByName(productName);
+		Integer sprintDuration = product.getSprintDuration();
+		
+		// Calcule de la date de début et de fin
+		java.util.Calendar cal = java.util.Calendar.getInstance();
+		java.util.Date utilDate = cal.getTime();
+		Date created = new Date(utilDate.getTime());
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(created);
+		
+		models.Version version = new models.Version(release, created);
+		for (int i = 0; i < nbSprints; i++) {
+			calendar.add(Calendar.DATE, sprintDuration);
+			Date finished = new Date(calendar.getTimeInMillis());
+			models.Sprint sprint = new models.Sprint(created, finished);
+			created = finished;
+			sprint.setRelease(version);
+			version.save();
+			sprint.register();
+			version.addSprint(sprint);
+		}
+		version.setProduct(product);
+		version.register();
+		
+		redirect("/Application/dashboard");
+	}
+}
