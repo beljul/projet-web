099e93621a73985d496ee253d64443cf4dca7363
kevanescence <kevanescence@hotmail.fr>
2014-05-23 16:00:15 +0200

    order-exigence

--- public/javascripts/requirement-order.js
+++ public/javascripts/requirement-order.js
@@ -1,2 +1,23 @@
 $( "#sortable" ).sortable();
 $( "#sortable" ).disableSelection();
+$("input#save-order").on("click",function(){
+	var requirementsId = [];	
+	$("#sortable").children().each(function(){
+		requirementsId.push($(this).attr("data-id"));
+	});	
+	$.ajax({
+		url: "/User/jsonSearch",
+		data:requirementsId;
+		beforeSend:function(){
+		}
+	})
+	.done(function() {
+	alert( "success" );
+	})
+	.fail(function() {
+	alert( "error" );
+	})
+	.always(function() {
+	alert( "complete" );
+	});
+});
