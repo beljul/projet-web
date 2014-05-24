a183ff8afe9c180750d39584b196537adfcdb069
kevanescence <kevanescence@hotmail.fr>
2014-05-24 18:11:24 +0200

    Add a javascript flash system

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -2,9 +2,10 @@ $(function () {
 	$('#tabs a').click(function (e) {
 		e.preventDefault();
 		$(this).tab('show');
-	})
+	});
 	
 	$('li.disabled a').attr("href", "#");
+	HTMLFlash_contextual("mevoici", "validation", false);
 })
   
 
--- public/javascripts/flash.js
+++ public/javascripts/flash.js
@@ -1,5 +1,18 @@
 $(function(){
-    var flash=$("#message-flash");
+	__HTMLFlash_initAnimation(false);
+});
+/**
+ * Build event on a evenutal flash message
+ * @param ajax true if called by an ajax success response
+ * @return
+ */
+function __HTMLFlash_initAnimation(ajax){
+	var flash;
+	if(ajax)
+		flash=$("div.flash");
+	else
+		flash=$("div.flash.ajax");
+	console.log(flash);
     if(flash.length > 0 ){
         if(flash.hasClass("auto-hide"))
             flash.hide().slideDown(1000).delay(5000).slideUp(1000);
@@ -9,6 +22,53 @@ $(function(){
                 e.preventDefault();
                 flash.slideUp(1000);
             })
-            }
+       }
     }
-});
+}
+
+/**
+ * Create a contextual message in the ul#scrum-menu
+ * @param message
+ * @param style    error, validation, warning or information
+ * @param closable if true, a link will be displayed to close the flash
+ * 					if false,the flash will disappeared automatically
+ * @return
+ */
+function HTMLFlash_contextual(message, style, closable){
+	__HTMLFlash_init(message, style, closable, window, $('ul#scrum-menu'));
+}
+
+/**
+ * Create a window flash message 
+ * @param message
+ * @param style    error, validation, warning or information
+ * @param closable if true, a link will be displayed to close the flash
+ * 					if false,the flash will disappeared automatically
+ * @return
+ */
+function HTMLFlash_screen(message, style, closable){
+	__HTMLFlash_init(message, style, closable, true, $('body'));
+}
+
+/**
+ * Initialize a flash message with the given parameters
+ * @param message
+ * @param style
+ * @param closable
+ * @param window if true, it will be a window message, contextual otherwise
+ * @param subject define in which element the message has to be prepend
+ * @return
+ */
+function __HTMLFlash_init(message, style, closable, window, subject) {
+	closable = closable===true?'':'auto-hide';
+	window = window === true?'window':'';
+	var html =	'<div class="ajax flash ' + window + ' '
+					+ style + ' ' + closable + '">'
+    if(closable) {
+    	html += '<a href="#" class="flash-close">x</a>';
+    }	 
+	html += message + '</div>';
+	var div = $(html).css('display','block');
+	subject.append(div);
+	__HTMLFlash_initAnimation(true);
+}
