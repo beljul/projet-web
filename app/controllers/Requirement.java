4093a3f866947f3450d393237dff08951f563848
beljul <jullian.bellino@gmail.com>
2014-05-25 17:26:08 +0200

    merge

--- app/controllers/Requirement.java
+++ app/controllers/Requirement.java
@@ -56,10 +56,12 @@ public class Requirement extends WrapperController {
 	}
 	
 	public static void getRequirementsUnassigned() {
+		// Get requirements of current product
 		String productName = session.get("productName");
 		models.Product product = models.Product.getByName(productName);
 		Set<models.Requirement> requirementsUnassigned = product.getRequirements();
 		
+		// Delete requirements if they are already assigned to a sprint
 		for (models.Version v : product.getReleases()) {
 			for (models.Sprint sp : v.getSprints()) {
 				requirementsUnassigned.removeAll(sp.getRequirements());
