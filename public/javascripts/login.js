50b5d4da011d28c6e8a1e210dea45baeae610936
beljul <jullian.bellino@gmail.com>
2014-06-04 21:11:11 +0200

    Validation login page (client)

---
+++ public/javascripts/login.js
@@ -0,0 +1,28 @@
+/*
+ * Verification of login form
+ */
+
+$(function () {
+	$("form[action='/user/register']").checkform({
+		CSSClass:"alert alert-danger",
+		items:{
+			password: {
+				selector:'[name=firstPassword], [name=secondPassword]'
+			}
+		}
+	});
+	$("form[action='/secure/authenticate']").checkform({
+		type:"flash",
+		CSSClass:"flash window flashError",
+		items:{
+			mail: {
+				selector:'[name=username]'
+			},
+			password: {
+				selector:'[name=password]'
+			}
+		}
+	});
+});
+
+
